ddfe670b2099464f69eef000115bb6f2c02770de
Fatmatrn <ftmtrn3488@gmail.com>
2023-06-10 15:38:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const firstSelectEl = document.getElementById("currency-first");
+const secondSelectEl = document.getElementById("currency-second");
+const firstInput = document.getElementById("first-value");
+const exchangeDescription = document.getElementById("exchange-description");
+const secondInput = document.getElementById("second-value");
+
+firstSelectEl.addEventListener("change", updateRate);
+secondSelectEl.addEventListener("change", updateRate);
+firstInput.addEventListener("input", updateRate);
+
+
+function updateRate(){
+    fetch(`https://v6.exchangerate-api.com/v6/656593d99f9f621e33d0f567/latest/${firstSelectEl.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+        let rate = data.conversion_rates[secondSelectEl.value];
+        secondInput.value = (rate * firstInput.value).toFixed(2);
+        exchangeDescription.innerText = `1 ${firstSelectEl.value} = ${rate + " " + secondSelectEl.value}`
+        
+    })
+}
+
+
+
+
+
+
+
+
